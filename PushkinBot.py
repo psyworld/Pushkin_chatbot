b15f1ab38ee41f70ff40001c219c65fe715bbba5
Denis-26 <kartavenkodenis0@gmail.com>
2017-12-02 12:28:31 +0300

    pushkin api

--- PushkinBot.py
+++ PushkinBot.py
@@ -1,9 +1,17 @@
 from Bot import telegram
+from pushkin_api import PushkinApi
 
 class PushkinBot(telegram.Bot):
     def __init__(self, config, loop=None):
         super().__init__(config, loop=loop)
+        self.loop.create_task(self.send_res())
 
     async def handler(self, update):
         user_id = update.message.user.id
         await self.api.send_message(user_id, "AAAAAAAAA")
+        await self.api.send_sticker(user_id, "CAADAgADOAEAAkJPOQABXRK1Mn5TOgsC")
+
+    async def send_res(self):
+        p_api = PushkinApi(self.loop)
+        r = await p_api.api_get("https://all.culture.ru/api/2.2/events?organizations=607&start=1512204775432&sort=start")
+        print(r)
